5c40dfc7a74b9083bb7cd6637066d4c01951b585
Stronx <stronx0192@gmail.com>
2022-09-08 21:47:11 +0200

    Version 1.0 WEB AEA

--- src/app/mockAviones.ts
+++ src/app/mockAviones.ts
@@ -1,8 +1,7 @@
 import {Avion} from "./Aviones";
 
 export const AVIONES: Avion[] = [
-  {id:0, modelo:'Boeing 787-9',imagen:''},
-  {id:0, modelo:'Boeing 787-8',imagen:''},
-  {id:0, modelo:'Boeing 737-800',imagen:''},
-  {id:0, modelo:'Embraer 195',imagen:''}
+  {id:0, modelo:'Boeing 787',imagen:'https://cdn.discordapp.com/attachments/1007386737229692938/1017511910679269377/Desktop_Screenshot_2020.09.08_-_01.13.23.82.png',desc:'El Boeing 787 Dreamliner es un avión bimotor de tamaño medio y fuselaje ancho, de doble pasillo, con capacidad para transportar entre 217 y 323 pasajeros, según su tipo (8, 9 o 10) y configuración. Alcanza una velocidad de 957 kilómetros por hora y utiliza motores General Electric o Rolls Royce.'},
+  {id:2, modelo:'Boeing 737-800',imagen:'https://cdn.discordapp.com/attachments/1007318186145873961/1013083658376204338/Microsoft_Flight_Simulator_Screenshot_2022.08.27_-_15.43.52.71.png',desc:'Es uno de los últimos modelos de la gama ‘737’ y ofrece la máxima fiabilidad. Sus reactores han sido los más demandados en toda la historia del sector de la aviación. Tiene una autonomía de casi 5.000 kilómetros y cuenta con 180 plazas, 12 de ellas en business.'},
+  {id:3, modelo:'Embraer 195',imagen:'https://upload.wikimedia.org/wikipedia/commons/8/8c/Air_Europa_Embraer_195_in_new_livery_at_Munich_Airport.jpeg',desc:'Se trata del más pequeño de nuestra flota y opera en Air Europa Express, es el modelo más grande del fabricante brasileño Embraer. Está configurado con 122 plazas y los once ejemplares que operamos cubren rutas domésticas y europeas.'}
 ]
